64c93a31f3ff2dc7530ca770c62feb708b89099c
bobbyzhong <3bobbyzhong3@gmail.com>
2024-02-17 14:56:31 -0800

    updates

--- src/app/components/SignInButton.tsx
+++ src/app/components/SignInButton.tsx
@@ -6,18 +6,17 @@ import { signIn } from "next-auth/react";
 type Props = { text: string };
 
 const SignInButton = ({ text }: Props) => {
-  return (
-    <Button
-
-      onClick={() => {
-        signIn("google", { callbackUrl: "/dashboard-student" }).catch(
-          console.error
-        );
-      }}
-    >
-      {text}
-    </Button>
-  );
+    return (
+        <Button
+            onClick={() => {
+                signIn("google", { callbackUrl: "/onboard" }).catch(
+                    console.error
+                );
+            }}
+        >
+            {text}
+        </Button>
+    );
 };
 
 export default SignInButton;
--- src/app/dashboard-student/page.tsx
+++ src/app/dashboard-student/page.tsx
@@ -1,27 +1,34 @@
 import { redirect } from "next/navigation";
 import Card from "../components/Card";
 
+import { getServerSession } from "next-auth";
+
 type Props = {};
 
 export const metadata = {
-  title: "Dashboard | Auntie",
+    title: "Dashboard | Auntie",
 };
 
-const Dashboard = (props: Props) => {
-  return (
-    <main className="p-8 mx-auto max-w-7xl">
-      <div className="flex items-center mt-12">
-        <div className="flex flex-col gap-1">
-          <h2 className="mr-2 text-[28px] font-bold tracking-tight">
-            Local Meals
-          </h2>
-          <h1 className="text-zinc-500 text-[15px] dark:text-zinc-300">
-            Find local meals from aunties nearby
-          </h1>
-          <Card />
-        </div>
-      </div>
-    </main>
-  );
+const Dashboard = async (props: Props) => {
+    const session = await getServerSession();
+
+    if (!session || !session.user) {
+        redirect("/login");
+    }
+
+    return (
+        <main className="p-8 mx-auto max-w-7xl">
+            <div className="flex items-center mt-12">
+                <div className="flex flex-col gap-1">
+                    <h2 className="mr-2 text-[28px] font-bold tracking-tight">
+                        Local Meals
+                    </h2>
+                    <h1 className="text-zinc-500 text-[15px] dark:text-zinc-300">
+                        Find local meals from aunties nearby
+                    </h1>
+                </div>
+            </div>
+        </main>
+    );
 };
 export default Dashboard;
