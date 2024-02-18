dcac0552f7bc69d1b669003852862c0c73a7ed28
tawsif-kamal <tawsif.kamal@ibm.com>
2024-02-18 00:29:34 -0800

    hardcoded

--- src/app/components/StudentMealList.tsx
+++ src/app/components/StudentMealList.tsx
@@ -1,72 +1,73 @@
 "use client";
-import { MealCard } from "./MealCard";
+import { MealCard } from "./mealCard";
 import { useState, useEffect } from "react";
 
 export const metadata = {
-    title: "Dashboard | Auntie",
+  title: "Dashboard | Auntie",
 };
 
 export default function StudentMealList({ session }: any) {
-    const [meals, setMeals] = useState<any[]>([]); // Using 'any' type for meals
+  const [meals, setMeals] = useState<any[]>([]); // Using 'any' type for meals
 
-    useEffect(() => {
-        // Fetch meals when the component mounts
-        fetchMeals()
-            .then((fetchedMeals) => setMeals(fetchedMeals.meals))
-            .catch((error) => console.error("Failed to fetch meals:", error));
-    }, []); // Empty dependency array to run the effect only once
+  useEffect(() => {
+    // Fetch meals when the component mounts
+    fetchMeals()
+      .then((fetchedMeals) => setMeals(fetchedMeals.meals))
+      .catch((error) => console.error("Failed to fetch meals:", error));
+  }, []); // Empty dependency array to run the effect only once
 
-    const fetchMeals = async () => {
-        try {
-            const response = await fetch("/api/meal");
-            if (!response.ok) {
-                throw new Error("Failed to fetch meals");
-            }
-            return await response.json();
-        } catch (error) {
-            throw new Error(`An error occurred while fetching meals: ${error}`);
-        }
+  const fetchMeals = async () => {
+    try {
+      const response = await fetch("/api/meal");
+      if (!response.ok) {
+        throw new Error("Failed to fetch meals");
+      }
+      return await response.json();
+    } catch (error) {
+      throw new Error(`An error occurred while fetching meals: ${error}`);
+    }
+  };
+
+  function formatDateString(isoString: Date) {
+    const date = new Date(isoString);
+
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long", // 'long', 'short', 'narrow'
+      hour: "numeric", // 'numeric', '2-digit'
+      minute: "numeric", // 'numeric', '2-digit'
+      hour12: true,
     };
 
-    function formatDateString(isoString: Date) {
-        const date = new Date(isoString);
+    // The 'en-US' locale is used as an example; you can adjust it to your needs
+    return new Intl.DateTimeFormat("en-US", options).format(date);
+  }
 
-        const options: Intl.DateTimeFormatOptions = {
-            weekday: "long", // 'long', 'short', 'narrow'
-            hour: "numeric", // 'numeric', '2-digit'
-            minute: "numeric", // 'numeric', '2-digit'
-            hour12: true,
-        };
+  function formatPrice(price: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price);
+  }
 
-        // The 'en-US' locale is used as an example; you can adjust it to your needs
-        return new Intl.DateTimeFormat("en-US", options).format(date);
-    }
-
-    function formatPrice(price: number) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-        }).format(price);
-    }
-
-    return (
-        <>
-            {meals.map((meal: any, index: any) => {
-                return (
-                    <MealCard
-                        name={meal.name}
-                        school={meal.school}
-                        meetTime={formatDateString(meal.meetTime)}
-                        price={formatPrice(meal.price)}
-                        capacity={meal.capacity}
-                        description={meal.description}
-                        imgSrc={meal.imageSrc}
-                        key={index}
-                        mealId={meal._id}
-                        session={session}
-                    />
-                );
-            })}
-        </>
-    );
+  return (
+    <>
+      {meals.map((meal: any, index: any) => {
+        return (
+          <MealCard
+            name={meal.name}
+            school={meal.school}
+            meetTime={formatDateString(meal.meetTime)}
+            price={formatPrice(meal.price)}
+            capacity={meal.capacity}
+            description={meal.description}
+            imgSrc={meal.imageSrc}
+            count={meal.count}
+            key={index}
+            mealId={meal._id}
+            session={session}
+          />
+        );
+      })}
+    </>
+  );
 }
--- src/app/components/mealCard.tsx
+++ src/app/components/mealCard.tsx
@@ -4,182 +4,167 @@ import { useToast } from "@/components/ui/use-toast";
 
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogClose,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
 } from "@/components/ui/dialog";
 
 import { DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
 export function MealCard({
-    name,
-    mealId,
-    description,
-    school,
-    meetTime,
-    studentIds,
-    imgSrc,
-    capacity,
-    session,
-    price,
+  name,
+  mealId,
+  description,
+  school,
+  meetTime,
+  studentIds,
+  imgSrc,
+  capacity,
+  session,
+  price,
+  count,
 }: any) {
-    const [isAlreadySubscribed, setIsAlreadySubscribed] =
-        useState<boolean>(false); // Using 'any' type for meals
+  const [isAlreadySubscribed, setIsAlreadySubscribed] =
+    useState<boolean>(false); // Using 'any' type for meals
 
-    const { toast } = useToast();
+  const { toast } = useToast();
 
-    useEffect(() => {
-        const checkIfMealSubscribed = async () => {
-            try {
-                const requestOptions = {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        mealId,
-                        userEmail: session.user.email,
-                    }),
-                };
-
-                const response = await fetch(
-                    `/api/meal/subscribe`,
-                    requestOptions
-                );
-
-                if (!response.ok) {
-                    throw new Error("Failed to fetch meals");
-                }
-
-                const { mealExists } = await response.json();
-
-                if (mealExists) {
-                    setIsAlreadySubscribed(true);
-                }
-            } catch (error) {
-                throw new Error(
-                    `An error occurred while fetching meals: ${error}`
-                );
-            }
+  useEffect(() => {
+    const checkIfMealSubscribed = async () => {
+      try {
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            mealId,
+            userEmail: session.user.email,
+          }),
         };
 
-        checkIfMealSubscribed();
-    }, []);
+        const response = await fetch(`/api/meal/subscribe`, requestOptions);
 
-    const subscribeToMeal = async () => {
-        try {
-            const requestOptions = {
-                method: "PUT", // or 'PUT'
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ mealId, userEmail: session.user.email }),
-            };
-
-            const response = await fetch("/api/meal/subscribe", requestOptions);
-
-            if (!response.ok) {
-                throw new Error("Failed to fetch meals");
-            }
-
-            setIsAlreadySubscribed(true);
-
-            toast({
-                title: "Your order has been placed!",
-                variant: "default",
-                duration: 2000,
-            });
-
-            return response.json();
-        } catch (error) {
-            throw new Error(`An error occurred while fetching meals: ${error}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch meals");
         }
+
+        const { mealExists } = await response.json();
+
+        if (mealExists) {
+          setIsAlreadySubscribed(true);
+        }
+      } catch (error) {
+        throw new Error(`An error occurred while fetching meals: ${error}`);
+      }
     };
 
-    const descriptionItems = [
-        {
-            logoSrc: "/location.png",
-            attribute: school,
+    checkIfMealSubscribed();
+  }, []);
+
+  const subscribeToMeal = async () => {
+    try {
+      const requestOptions = {
+        method: "PUT", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-            logoSrc: "/clock.png",
-            attribute: meetTime,
-        },
-        {
-            logoSrc: "/dollar.png",
-            attribute: price,
-        },
-    ];
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Card
-                    className={`${
-                        isAlreadySubscribed && "border border-green-500"
-                    } rounded-tl-2xl rounded-tr-2xl overflow-hidden h-4/6 relative transition-all 
+        body: JSON.stringify({ mealId, userEmail: session.user.email }),
+      };
+
+      const response = await fetch("/api/meal/subscribe", requestOptions);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch meals");
+      }
+
+      setIsAlreadySubscribed(true);
+
+      toast({
+        title: "Your order has been placed!",
+        variant: "default",
+        duration: 2000,
+      });
+
+      return response.json();
+    } catch (error) {
+      throw new Error(`An error occurred while fetching meals: ${error}`);
+    }
+  };
+
+  const descriptionItems = [
+    {
+      logoSrc: "/location.png",
+      attribute: school,
+    },
+    {
+      logoSrc: "/clock.png",
+      attribute: meetTime,
+    },
+    {
+      logoSrc: "/dollar.png",
+      attribute: price,
+    },
+  ];
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Card
+          className={`${
+            isAlreadySubscribed && "border border-green-500"
+          } rounded-tl-2xl rounded-tr-2xl overflow-hidden h-4/6 relative transition-all 
                     duration-300 ease-in-out
                      hover:shadow-lg hover:cursor-pointer`}
-                >
-                    <img
-                        src={imgSrc}
-                        alt="food of image"
-                        className="w-full h-2/5 object-cover "
-                    />
-                    <CardTitle className="px-4 pt-4 text-xl">{name}</CardTitle>
-                    <CardDescription className="px-4 py-2">
-                        {description}
-                    </CardDescription>
-                    <div className="px-4 py-2">
-                        {descriptionItems.map((item, index) => {
-                            return (
-                                <p
-                                    className="flex gap-2 items-center"
-                                    key={index}
-                                >
-                                    <img
-                                        src={item.logoSrc}
-                                        className="h-4 w-4"
-                                    />
-                                    {item.attribute}
-                                </p>
-                            );
-                        })}
-                    </div>
+        >
+          <img
+            src={imgSrc}
+            alt="food of image"
+            className="w-full h-2/5 object-cover "
+          />
+          <CardTitle className="px-4 pt-4 text-xl">{name}</CardTitle>
+          <CardDescription className="px-4 py-2">{description}</CardDescription>
+          <div className="px-4 py-2">
+            {descriptionItems.map((item, index) => {
+              return (
+                <p className="flex gap-2 items-center" key={index}>
+                  <img src={item.logoSrc} className="h-4 w-4" />
+                  {item.attribute}
+                </p>
+              );
+            })}
+          </div>
 
-                    <p className="absolute right-2 bottom-2 text-xs">
-                        {" "}
-                        Sign-ups: {} / 3
-                    </p>
-                </Card>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[350px] h-[150px]">
-                <DialogHeader>
-                    <DialogTitle>Do you want to place your order?</DialogTitle>
-                    <DialogDescription></DialogDescription>
-                </DialogHeader>
+          <p className="absolute right-2 bottom-2 text-xs">
+            {" "}
+            Sign-ups: 2 / 3
+          </p>
+        </Card>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[350px] h-[150px]">
+        <DialogHeader>
+          <DialogTitle>Do you want to place your order?</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
 
-                <DialogFooter className="flex mr-6">
-                    <DialogClose>
-                        <Button className="w-16 m-0" variant="outline">
-                            No
-                        </Button>
-                    </DialogClose>
+        <DialogFooter className="flex mr-6">
+          <DialogClose>
+            <Button className="w-16 m-0" variant="outline">
+              No
+            </Button>
+          </DialogClose>
 
-                    <DialogClose>
-                        <Button
-                            className="w-16 rounded"
-                            onClick={() => subscribeToMeal()}
-                        >
-                            Yes
-                        </Button>
-                    </DialogClose>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
+          <DialogClose>
+            <Button className="w-16 rounded" onClick={() => subscribeToMeal()}>
+              Yes
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
