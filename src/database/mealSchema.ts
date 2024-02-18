aefd9438c602461ac8fbd33a2f6440e1de4bb859
bobbyzhong <3bobbyzhong3@gmail.com>
2024-02-18 00:11:54 -0800

    udpates

--- src/database/mealSchema.ts
+++ src/database/mealSchema.ts
@@ -1,13 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 const MealSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  school: { type: String, required: true },
-  meetTime: { type: Date, required: true },
-  studentIds: { type: Array, required: true },
-  capacity: { type: Number, required: true },
-  price: { type: Number, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    school: { type: String, required: true },
+    meetTime: { type: Date, required: true },
+    studentIds: { type: Array, required: true },
+    capacity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    count: { type: Number, required: true },
 });
 
 export default mongoose.models.Meal || mongoose.model("Meal", MealSchema);
